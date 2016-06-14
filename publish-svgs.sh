123b39fd97282e81737c6e08bbfeb563a6e53ce5
R. Tyler Croy <tyler@monkeypox.org>
2016-06-14 14:47:25 -0700

    Place the generated SVGs in the right place

--- publish-svgs.sh
+++ publish-svgs.sh
@@ -3,7 +3,7 @@
 git checkout gh-pages
 git config user.name `hostname`
 git config user.email "no-reply@jenkins.io"
-cp -R target/svg/* svg/
-git add svg
+cp -R target/svg/* jenkins-stats/svg/
+git add jenkins-stats/svg
 git commit -am "Generating stats" || true
 git push git@github.com:jenkins-infra/infra-statistics.git gh-pages
