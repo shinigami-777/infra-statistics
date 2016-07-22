f00a00d3cab7428b9c7bd965dc490d95c1a025d8
R. Tyler Croy <tyler@monkeypox.org>
2016-07-22 13:21:49 -0700

    Grab a fresh gh-pages branch on each run

--- publish-svgs.sh
+++ publish-svgs.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -xe
 
-git checkout gh-pages
+# Delete our local branch if it exists
+git branch -D gh-pages || true
+# Check out the most recent version of the gh-pages branch
+git fetch origin
+git checkout gh-pages --track origin/gh-pages
+
 git config user.name `hostname`
 git config user.email "no-reply@jenkins.io"
+
 cp -R target/svg/* jenkins-stats/svg/
 cp -R target/stats/* plugin-installation-trend/
+
 git add jenkins-stats/svg
 git add plugin-installation-trend
+
 git commit -am "Generating stats" || true
 git push git@github.com:jenkins-infra/infra-statistics.git gh-pages
