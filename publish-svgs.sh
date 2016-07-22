984d26736f23add8979bd1590359bf7dcb481e74
R. Tyler Croy <tyler@monkeypox.org>
2016-07-22 16:54:18 -0700

    Re-order the generation of the index to make sure it executes before branch switching

--- publish-svgs.sh
+++ publish-svgs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -xe
 
+./generate-index.sh ./target/stats/
+
 # Delete our local branch if it exists
 git branch -D gh-pages || true
 # Check out the most recent version of the gh-pages branch
@@ -11,8 +13,6 @@ git config user.email "no-reply@jenkins.io"
 
 cp -R target/svg/* jenkins-stats/svg/
 cp -R target/stats/* plugin-installation-trend/
-
-./generate-index.sh plugin-installation-trend
 
 git add jenkins-stats/svg
 git add plugin-installation-trend
