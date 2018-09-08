c3b258f60408c03aaee2977e4f7d423f39506aba
Tobias Gruetzmacher <tobias-git@23.gs>
2018-09-08 21:51:44 +0200

    Move CSS and JS into external files

--- generateVersionDistribution.groovy
+++ generateVersionDistribution.groovy
@@ -1,15 +1,29 @@
 #!/usr/bin/env groovy
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+
+Path indir = Paths.get("pluginversions-static")
+Path outdir = Paths.get("target/pluginversions")
+
+Files.createDirectories(outdir)
+
+Files.list(indir).each { file ->
+    Files.copy(file, outdir.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING)
+}
 
 File file = new File("target/stats", "jenkins-version-per-plugin-version.json")
 
-def json = new JsonSlurper().parseText(file.text)."jenkins-version-per-plugin-version";
+def json = new JsonSlurper().parse(file)."jenkins-version-per-plugin-version";
 
+def htmlTemplate = new File("generateVersionDistribution-template.html").text
 json.each { plugin, versions ->
-    File out = new File("target/pluginversions", plugin + ".html")
-    out.parentFile.mkdirs()
-    out.text = new File("generateVersionDistribution-template.html").text.replace("__NAME__", plugin).replace("__DATA__", new JsonBuilder(versions).toString())
+    File out = new File(outdir.toFile(), plugin + ".html")
+
+    out.text = htmlTemplate.replaceAll("__NAME__", plugin).replace("__DATA__", new JsonBuilder(versions).toString())
 }
 
 "./generate-pluginversions-index.sh target/pluginsversions".execute()
---
+++ pluginversions-static/pluginversions.js
@@ -0,0 +1,79 @@
+
+function parseData(versionData, name) {
+    var pluginVersionsSet = new Set();
+
+    var coreVersionsSet = new Set();
+
+    var totalInstallsPerPluginVersion = new Map();
+
+    var totalInstalls = 0
+
+    for (var pluginVersion in versionData) {
+        if (/^\d[\d.]*\d$/.test(pluginVersion)) {
+            pluginVersionsSet.add(pluginVersion);
+            if (!totalInstallsPerPluginVersion.has(pluginVersion)) {
+                totalInstallsPerPluginVersion[pluginVersion] = 0;
+            }
+            for (var coreVersion in versionData[pluginVersion]) {
+                if (/^[12][.]\d+(|[.]\d)$/.test(coreVersion)) {
+                    coreVersionsSet.add(coreVersion);
+                    totalInstalls += versionData[pluginVersion][coreVersion];
+                    totalInstallsPerPluginVersion[pluginVersion] += versionData[pluginVersion][coreVersion];
+                }
+            }
+        }
+    }
+
+    var collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+
+    var pluginVersions = Array.from(pluginVersionsSet);
+    pluginVersions.sort(collator.compare);
+
+    var coreVersions = Array.from(coreVersionsSet);
+    coreVersions.sort(collator.compare);
+
+    var thisCoreVersionOrOlderPerPluginVersion = {};
+
+    // header row
+    var row = $("<tr>");
+    row.append($("<th>").html(name + " - " + totalInstalls));
+    for (let pluginVersion of pluginVersions) {
+        row.append($("<th>").html(pluginVersion));
+        thisCoreVersionOrOlderPerPluginVersion[pluginVersion] = 0;
+    }
+    row.append($("<th>").html("Sum"));
+    row.appendTo('#versionsContainer');
+
+
+    var thisCoreVersionOrOlder = 0
+
+    // value rows
+    for (let coreVersion of coreVersions) {
+        var row = $("<tr>");
+        if (/^\d[.]\d+[.]\d$/.test(coreVersion)) {
+            row.addClass('lts');
+        }
+        row.append($("<th>").html(coreVersion));
+        var thisCoreVersion = 0;
+        for (let pluginVersion of pluginVersions) {
+            var cnt = versionData[pluginVersion][coreVersion];
+            if (cnt == null) {
+                cnt = 0;
+            }
+            thisCoreVersion += cnt;
+            var title = pluginVersion + " on " + coreVersion + ": " + cnt + " installs (" + Math.round(cnt/totalInstalls*100) + "%)";
+            title += " - " + Math.round((1-thisCoreVersionOrOlderPerPluginVersion[pluginVersion]/totalInstallsPerPluginVersion[pluginVersion])*100) + "% of " + pluginVersion + " installs are on this core version or newer";
+            row.append($("<td>").attr("title", title).css("opacity", Math.max(0.1, cnt*100/totalInstalls)).html(cnt));
+
+            thisCoreVersionOrOlderPerPluginVersion[pluginVersion] += cnt;
+        }
+
+        var title = coreVersion + " total: " + thisCoreVersion + " installs (" + Math.round(thisCoreVersion/totalInstalls*100) + "%)";
+        title += " - " + Math.round((1-thisCoreVersionOrOlder/totalInstalls)*100) + "% of plugin installs are on this core version or newer";
+        row.append($("<td>").addClass("subtotal").attr("title", title).html(thisCoreVersion));
+
+        thisCoreVersionOrOlder += thisCoreVersion;
+
+        row.appendTo('#versionsContainer');
+    }
+}
