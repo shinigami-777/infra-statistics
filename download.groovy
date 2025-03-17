de4c09de03dd035576f92eb49d418711d02f5255
Tamaghna Chattopadhyay <139495942+shinigami-777@users.noreply.github.com>
2025-03-17 14:11:33 +0530

    refactored code for Downloader class

--- download.groovy
+++ download.groovy
@@ -1,9 +1,6 @@
 #!/usr/bin/env groovy
-import java.io.File
-import java.util.zip.GZIPInputStream
 import groovyx.net.http.ContentType
 import groovyx.net.http.HTTPBuilder
-
 
 @Grapes([
     @Grab(group='org.codehaus.jackson', module='jackson-mapper-asl', version='1.9.13'),
@@ -21,51 +18,66 @@ class Downloader {
     def db
     def workingDir
 
-    def Downloader(workingDir, db){
+    Downloader(workingDir, db){
         this.db = db
         this.workingDir = workingDir
     }
 
-    /**
-     * gets all compressed JSON files from jenkins-ci
-     */
-    def getFiles(pwd) {
-		
-		println("get files from $authUrl")
-		
-        def site = new HTTPBuilder( authUrl )
-        // site.auth.basic 'jenkins', pwd - this did not work, therefore using header :(
-	    def userAuth = "jenkins:$pwd".toString()
-        def doc = site.get( path:'/census/', headers:[Authorization:"Basic ${userAuth.bytes.encodeBase64()}"])
+    // gets all compressed JSON files from jenkins-ci
 
-        doc.depthFirst().collect { it }.findAll {
-            it.name() == "A"
-        }.each {
-            def fileName = it.attributes()["href"]
-            if(fileName.endsWith(".json.gz")){
-                if(DBHelper.doImport(db, fileName)){
-                    def fileUrl = '/census/'+fileName
-                    def targetArchive = new File(workingDir, fileName)
-                    if (targetArchive.exists()) {
-                        println "ignore $fileName (already exists)"
-                    } else {
-                        println "download $fileUrl"
-                        def tmp = new File(workingDir,fileName+".tmp");
-                        tmp << site.get(contentType: ContentType.BINARY, path: fileUrl, headers:[Authorization:"Basic ${userAuth.bytes.encodeBase64()}"] ) // java.io.ByteArrayInputStream
-                        tmp.renameTo(targetArchive);
-                    }
-                } else{
-                    println "ignore $fileName (already imported)"
+    void getFiles(username, pwd) {
+
+        println("get files from $authUrl")
+        def site = new HTTPBuilder(authUrl)
+        def userAuth = "$username:$pwd".toString()
+        def authHeaders = "Basic ${userAuth.bytes.encodeBase64()}"
+
+        try {
+            def doc = site.get(path: '/census/', headers: [Authorization: authHeaders])
+
+            if (doc.toString().contains("308 Permanent Redirect")) {
+                println("Error: Incorrect password provided. Access denied.")
+                return
+            }
+
+            doc.depthFirst().collect { it }.findAll {
+                it.name() == "A"
+            }.each {
+                def fileName = it.attributes()["href"]
+                if (fileName.endsWith(".json.gz")) {
+                    handleFileDownload(site, fileName, authHeaders)
                 }
             }
+        } catch (Exception e) {
+            println ("Error: Unable to fetch files - ${e.message}")
         }
+    }
+
+    private void handleFileDownload(HTTPBuilder site, String filename, String authHeaders){
+
+        if(!DBHelper.doImport(db,filename)){
+            println ("ignore $fileName (already imported)")
+        }
+
+        File targetArchive = new File(workingDir, filename)
+        if(targetArchive.exists()){
+            println ("ignore $fileName (already exists)")
+        }
+
+        def fileUrl = '/census/'+fileName
+        println ("download $fileUrl")
+        File tmp = new File(workingDir,fileName+".tmp")
+        tmp << site.get(contentType: ContentType.BINARY, path: fileUrl, headers:[Authorization:authHeaders] )
+        tmp.renameTo(targetArchive)
+
     }
 
     def run(args) {
         if(args.size() != 1){
             println "no password for $authUrl given..."
         }else{
-            getFiles(args[0])
+            def username = "jenkins"
+            getFiles(username, args[0])
         }
     }
 }
