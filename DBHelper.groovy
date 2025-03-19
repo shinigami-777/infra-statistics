1c7adecb3f17f9aa01c4ef24726a203303889a5e
Tamaghna Chattopadhyay <139495942+shinigami-777@users.noreply.github.com>
2025-03-19 15:22:09 +0530

    refactored and enhanced DBHelper.groovy

--- DBHelper.groovy
+++ DBHelper.groovy
@@ -1,41 +1,39 @@
-import org.sqlite.*
-import java.sql.*
+import groovy.sql.Sql
 
 class DBHelper {
+    private static final String DB_NAME = "stats.db"
 
-    def static setupDB(workingDir){
-
-        def dbFile = new File(workingDir, "stats.db")
-
+    static Sql setupDB(workingDir) {
+        def dbFile = new File(workingDir, DB_NAME)
         boolean dbExists = dbFile.exists()
 
-        // in memory
-        // db = groovy.sql.Sql.newInstance("jdbc:sqlite::memory:","org.sqlite.JDBC")
-
-        // persistent
-        def db = groovy.sql.Sql.newInstance("jdbc:sqlite:"+dbFile.absolutePath,"org.sqlite.JDBC")
-
-        if(!dbExists){
-            // define the tables
-            db.execute("create table jenkins(instanceid, month, version, jvmvendor, jvmname, jvmversion)")
-            db.execute("create table plugin(instanceid, month, name, version)")
-            db.execute("create table job(instanceid, month, type, jobnumber)")
-            db.execute("create table node(instanceid, month, osname, nodenumber)")
-            db.execute("create table executor(instanceid, month, numberofexecutors)")
-            db.execute("create table importedfile(name)")
-            db.execute("CREATE INDEX plugin_name on plugin (name)")
-            db.execute("CREATE INDEX jenkins_version on jenkins (version)")
-            db.execute("CREATE INDEX plugin_month on plugin (month)")
-            db.execute("CREATE INDEX plugin_namemonth on plugin (name,month)")
+        Sql db = Sql.newInstance("jdbc:sqlite:" + dbFile.absolutePath, "org.sqlite.JDBC")
+        if (!dbExists) {
+            createTables(db)
+        }
+        else{
+            println("Database does not exist")
         }
 
-        return db;
+        return db
     }
 
+    // Create tables if database exists
+    private static void createTables(Sql db) {
 
-    /**
-     * is the file with the given name already imported?
-     */
+        db.execute("create table jenkins(instanceid, month, version, jvmvendor, jvmname, jvmversion)")
+        db.execute("create table plugin(instanceid, month, name, version)")
+        db.execute("create table job(instanceid, month, type, jobnumber)")
+        db.execute("create table node(instanceid, month, osname, nodenumber)")
+        db.execute("create table executor(instanceid, month, numberofexecutors)")
+        db.execute("create table importedfile(name)")
+        db.execute("CREATE INDEX plugin_name on plugin (name)")
+        db.execute("CREATE INDEX jenkins_version on jenkins (version)")
+        db.execute("CREATE INDEX plugin_month on plugin (month)")
+        db.execute("CREATE INDEX plugin_namemonth on plugin (name,month)")
+    }
+
+    // Check if the file with the given name already imported?
     static boolean doImport(db, fileName){
         if(db){
             def filePrefix = fileName.substring(0, fileName.indexOf("."))+"%"
@@ -44,8 +42,4 @@ class DBHelper {
         }
         true
     }
-
 }
-
-
-
